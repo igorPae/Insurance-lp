58844e0f48be012b4cf0ec0f3f9a742447b90354
afain77 <afinshtein@gmail.com>
2018-03-09 14:58:04 +0600

    New folder

---
+++ version2/send-email.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * This example shows settings to use when sending via Google's Gmail servers.
+ * This uses traditional id & password authentication - look at the gmail_xoauth.phps
+ * example to see how to use XOAUTH2.
+ * The IMAP section shows how to save this message to the 'Sent Mail' folder using IMAP commands.
+ */
+//Import PHPMailer classes into the global namespace
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require("/home/dmdgrmupahs9/public_html//mailer/src/Exception.php");
+require("/home/dmdgrmupahs9/public_html//mailer/src/PHPMailer.php");
+require("/home/dmdgrmupahs9/public_html//mailer/src/SMTP.php");
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 2;
+//Set the hostname of the mail server
+$mail->isSMTP();
+$mail->Host = 'n3plcpnl0064.prod.ams3.secureserver.net';
+$mail->Port = 465;
+$mail->SMTPAuth = false;
+$mail->SMTPSecure = false;
+
+// use
+// $mail->Host = gethostbyname('smtp.gmail.com');
+// if your network does not support SMTP over IPv6
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+//Set the encryption system to use - ssl (deprecated) or tls
+//Whether to use SMTP authentication
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "admin@fed-dev.net";
+//Password to use for SMTP authentication
+$mail->Password = "Ss221185";
+//Set who the message is to be sent from
+$mail->setFrom('admin@fed-dev.net', 'First Last');
+//Set an alternative reply-to address
+$mail->addReplyTo('replyto@example.com', 'First Last');
+//Set who the message is to be sent to
+$mail->addAddress('admin@fed-dev.net', 'John Doe');
+//Set the subject line
+$mail->Subject = 'PHPMailer GMail SMTP test';
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+//Replace the plain text body with one created manually
+$mail->AltBody = 'This is a plain-text message body';
+//Attach an image file
+$mail->msgHTML("test body"); //$mail->msgHTML(file_get_contents('contents.html'), __DIR__); //Read an HTML message body from an external file, convert referenced images to embedded,
+$mail->AltBody = 'HTML messaging not supported';
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+    //Section 2: IMAP
+    //Uncomment these to save your message in the 'Sent Mail' folder.
+    #if (save_mail($mail)) {
+    #    echo "Message saved!";
+    #}
+}
+//Section 2: IMAP
+//IMAP commands requires the PHP IMAP Extension, found at: https://php.net/manual/en/imap.setup.php
+//Function to call which uses the PHP imap_*() functions to save messages: https://php.net/manual/en/book.imap.php
+//You can use imap_getmailboxes($imapStream, '/imap/ssl') to get a list of available folders or labels, this can
+//be useful if you are trying to get this working on a non-Gmail IMAP server.
